f6e3022be440d243dd4abcbd25785b3092b64bec
Alex Shen <swlsww@hotmail.com>
2021-04-07 21:04:52 +0800

    reformat

--- merge_excel.py
+++ merge_excel.py
@@ -22,7 +22,8 @@ def last_non_empty_col(sheet, row, col):
 
 
 def copy_range(src_ws: xlrd.sheet, src_row, src_col_start, src_col_end,
-               dest_ws: openpyxl.worksheet.worksheet.Worksheet, dest_row, dest_col):
+               dest_ws: openpyxl.worksheet.worksheet.Worksheet, 
+               dest_row, dest_col):
     '''
     copy the source range to the destination range
     returns True if any non empty cell is copied
@@ -41,10 +42,12 @@ def copy_range(src_ws: xlrd.sheet, src_row, src_col_start, src_col_end,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'source', nargs='+', help='source workbook path, simple unix shell style pattern can be used')
-    parser.add_argument(
-        '--top-left', metavar='ROW,COL', type=cell_address, help='the top left cell of the range in each worksheet to merge, i.e. row,col')
+    parser.add_argument('source', nargs='+',
+                        help='source workbook path, simple unix shell style \
+                              pattern can be used')
+    parser.add_argument('--top-left', metavar='ROW,COL', type=cell_address,
+                        help='the top left cell of the range in \
+                              each worksheet to merge, i.e. row,col')
     parser.add_argument('--header', action='store_true', default=False,
                         help='whether each range has a header row')
     parser.add_argument('-w', type=int, dest='width',
@@ -71,8 +74,9 @@ if __name__ == '__main__':
                     first = False
                 else:
                     row += 1
-            while row < ws.nrows and copy_range(ws, row, col, col + range_width - 1, out_ws,
-                                                out_row, 1):
+            while row < ws.nrows and \
+                    copy_range(ws, row, col, col + range_width - 1,
+                               out_ws, out_row, 1):
                 row += 1
                 out_row += 1
     output.save(args.output)
