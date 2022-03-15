1ec7b00eef024c981ce1b5aa2f07e596682fefec
Alex Shen <swlsww@hotmail.com>
2022-03-15 10:12:00 +0800

    replace xlrd with openpyxl

--- merge_excel.py
+++ merge_excel.py
@@ -1,27 +1,16 @@
 #! /usr/bin/env python3
 
-import os
 import glob
 import argparse
 import openpyxl
-import xlrd
 
 
 def cell_address(s):
     return tuple(int(e) for e in s.split(','))
 
 
-def last_non_empty_col(sheet, row, col):
-    '''
-    find the last non empty cell in the consecutive non empty range starting at
-    (row, col)
-    '''
-    while col + 1 < sheet.ncols and sheet.cell(row, col + 1).value:
-        col += 1
-    return col
-
-
-def copy_range(src_ws: xlrd.sheet, src_row, src_col_start, src_col_end,
+def copy_range(src_ws: openpyxl.worksheet.worksheet.Worksheet,
+               src_row, src_col_start, src_col_end,
                dest_ws: openpyxl.worksheet.worksheet.Worksheet, 
                dest_row, dest_col):
     '''
@@ -49,7 +38,7 @@ if __name__ == '__main__':
                         help='the top left cell of the range in \
                               each worksheet to merge, i.e. row,col')
     parser.add_argument('--header', action='store_true', default=False,
-                        help='whether each range has a header row')
+                        help='if the first row is a header row')
     parser.add_argument('-w', type=int, dest='width',
                         help='width of the range to merge')
     parser.add_argument('-o', required=True, dest='output',
@@ -63,18 +52,16 @@ if __name__ == '__main__':
     out_row = 1
     for p in args.source:
         for f in glob.glob(p):
-            ws = xlrd.open_workbook(f).sheet_by_index(0)
+            ws = openpyxl.load_workbook(f).worksheets[0]
             row, col = args.top_left
-            row -= 1
-            col -= 1
             if not range_width:
-                range_width = last_non_empty_col(ws, row, col) - col + 1
+                range_width = ws.max_column - col + 1
             if args.header:
                 if first:
                     first = False
                 else:
                     row += 1
-            while row < ws.nrows and \
+            while row <= ws.max_row and \
                     copy_range(ws, row, col, col + range_width - 1,
                                out_ws, out_row, 1):
                 row += 1
