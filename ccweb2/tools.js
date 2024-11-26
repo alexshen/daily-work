e97e155ce2cedf8a606c13797902edc1440b5e9e
shenweihui <swlsww@hotmail.com>
2024-11-26 16:13:33 +0800

    only dump user tags

--- ccweb2/tools.js
+++ ccweb2/tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ccweb2 tools
 // @namespace    https://github.com/alexshen/daily-work/ccweb2
-// @version      0.33
+// @version      0.34
 // @description  Tools for cc web 2
 // @author       ashen
 // @match        https://jczl.sh.cegn.cn/web/*
@@ -178,10 +178,12 @@
                 );
                 const personInfo = _.chain(resp)
                     .pick(["name", "phone", "cardNo", "hjdz", "personId", "personType", 
-                           "tags", "relId", "liveStatus", "emergencyContact", "emergencyPhone"])
+                           "relId", "liveStatus", "emergencyContact", "emergencyPhone"])
                     .merge(_.pick(basicPersonInfo, "jzdz"))
                     .update('hjdz', clean)
                     .value();
+                // tagType 2 seems internal
+                personInfo.tags = _.chain(resp.tagList).filter({ "tagType": "0" }).map('tagName').join(',').value();
                 return csvConv.convertToArray(personInfo);
             }, 10, async () => await cc.delay(100)));
         }
