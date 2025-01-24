327d009f2c243abf3c2552e30e674ed824aa20c3
shenweihui <swlsww@hotmail.com>
2025-01-24 14:55:42 +0800

    bump api version

--- ccweb2/tools.js
+++ ccweb2/tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ccweb2 tools
 // @namespace    https://github.com/alexshen/daily-work/ccweb2
-// @version      0.34
+// @version      0.35
 // @description  Tools for cc web 2
 // @author       ashen
 // @match        https://jczl.sh.cegn.cn/web/*
