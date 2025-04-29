350e47d167c6bef935766a79da689c8b1963f642
shenweihui <swlsww@hotmail.com>
2025-04-29 13:23:14 +0800

    dump all tags but without duplication

--- ccweb2/tools.js
+++ ccweb2/tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ccweb2 tools
 // @namespace    https://github.com/alexshen/daily-work/ccweb2
-// @version      0.35
+// @version      0.36
 // @description  Tools for cc web 2
 // @author       ashen
 // @match        https://jczl.sh.cegn.cn/web/*
@@ -182,8 +182,7 @@
                     .merge(_.pick(basicPersonInfo, "jzdz"))
                     .update('hjdz', clean)
                     .value();
-                // tagType 2 seems internal
-                personInfo.tags = _.chain(resp.tagList).filter({ "tagType": "0" }).map('tagName').join(',').value();
+                personInfo.tags = _.chain(resp.tagList).uniqBy(['tagName']).sortBy(['tagName']).map('tagName').join(',').value();
                 return csvConv.convertToArray(personInfo);
             }, 10, async () => await cc.delay(100)));
         }
