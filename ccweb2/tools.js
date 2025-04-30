40743e347e3f939b8f3ed1be5587d81571cfebab
shenweihui <swlsww@hotmail.com>
2025-04-30 13:29:44 +0800

    update referencing url to common.js

--- ccweb2/tools.js
+++ ccweb2/tools.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         ccweb2 tools
 // @namespace    https://github.com/alexshen/daily-work/ccweb2
-// @version      0.36
+// @version      0.37
 // @description  Tools for cc web 2
 // @author       ashen
 // @match        https://jczl.sh.cegn.cn/web/*
-// @require      https://raw.githubusercontent.com/alexshen/daily-work/main/ccweb2/common.js
+// @require      https://github.com/alexshen/daily-work/raw/main/ccweb2/common.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js
